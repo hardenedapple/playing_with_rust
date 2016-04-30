eaf6629da874d1e9ee90c4d9f216697af289c77b
hardenedapple <hardenedapple@server.fake>
2016-04-30 19:38:55 +0100

    Abstracted away checking two solutions are equal
    This was done by implementing PartialEq for my KnapsackSolution
    structure.
    The implementation used a seperate function that checks two vectors have
    exactly the same elements.

--- src/lib.rs
+++ src/lib.rs
@@ -15,13 +15,55 @@ pub struct KnapsackProblem {
     options: Vec<Item>,
 }
 
-#[derive(Debug,PartialEq,Eq)]
+#[derive(Debug)]
 pub struct KnapsackSolution {
     weight: u32,
     capacity: u32,
     value: u32,
     items: Vec<Item>,
 }
+
+fn vector_same_set<T: PartialEq + Clone>(left: &Vec<T>, right: &Vec<T>)
+    -> bool {
+
+    if left.len() != right.len() {
+        return false;
+    }
+
+    let mut temp_left = left.to_vec();
+    let mut temp_right = left.to_vec();
+
+    for item in temp_left.drain(..) {
+        let mut found_item = false;
+        // This initialisation is not required for the consistency of the program
+        // It's just here to make the compiler happy.
+        // I expect that's an indication bad style, but I'll worry about that later.
+        let mut position = 0;
+        for (index, value) in temp_right.iter().enumerate() {
+            if *value == item { found_item = true; position = index; break; }
+        }
+
+        if found_item {
+            temp_right.swap_remove(position);
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+impl PartialEq for KnapsackSolution {
+    fn eq(&self, other: &Self) -> bool {
+        let attributes_same =
+            self.weight == other.weight &&
+            self.capacity == other.capacity &&
+            self.value == other.value;
+        vector_same_set(&self.items, &other.items) && attributes_same
+    }
+}
+
+impl Eq for KnapsackSolution {}
 
 pub fn best_knapsack(mut problem: KnapsackProblem)
     -> Result<KnapsackSolution, Vec<Item>> {
@@ -80,7 +122,6 @@ mod tests {
             weight: 0, value: 0, capacity: 0, items: Vec::new(),
         };
         assert_eq!(knapsack_solution, correct_solution);
-        assert!(true);
     }
 
     #[test]
@@ -97,22 +138,30 @@ mod tests {
             capacity: 15,
             options: item_options.clone(),
         };
-        let other_items = knapsack_problem.options.clone();
-        let knapsack_solution = best_knapsack(knapsack_problem).unwrap();
-        assert_eq!(knapsack_solution.weight, 8);
-        assert_eq!(knapsack_solution.value, 15);
-        assert_eq!(knapsack_solution.capacity, 7);
-        for item in &other_items[1..] {
-            assert!(knapsack_solution.items.contains(&item));
-        }
+        let correct_solution = KnapsackSolution {
+            weight: 8,
+            value: 15,
+            capacity: 7,
+            items: item_options[1..].to_vec(),
+        };
 
-
+        assert_eq!(best_knapsack(knapsack_problem).unwrap(), correct_solution);
     }
 
     // #[test]
     // fn order_insensitive() {
     //     let item_options = // Generate random vector
     //         ;
+    //     let mut all_permutations = item_options.permutations();
+    //     let first_solution = all_permutations.next();
+    //     for solution in all_permutations {
+    //         assert_eq!(solution.weight, 8);
+    //         assert_eq!(solution.value, 15);
+    //         assert_eq!(solution.capacity, 7);
+    //         for item in &other_items[1..] {
+    //             assert!(solution.items.contains(&item));
+    //         }
+    //     }
     // }
 
 }
