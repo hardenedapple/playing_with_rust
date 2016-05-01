2f3ad6aa938cbf9ee9125df55b1ad3277b8f2be6
hardenedapple <hardenedapple@server.fake>
2016-05-01 21:07:27 +0100

    Working on tests, and consequently fixed one bug

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,13 @@
  * vimcmd: set makeprg=cargo\ test
  * vimcmd: !cargo test
  */
-
-#[derive(Debug,Clone,PartialEq,Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Item {
     weight: u32,
     value: u32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct KnapsackProblem {
     capacity: u32,
     options: Vec<Item>,
@@ -23,7 +22,7 @@ pub struct KnapsackSolution {
     items: Vec<Item>,
 }
 
-fn vector_same_set<T: PartialEq + Clone>(left: &Vec<T>, right: &Vec<T>)
+pub fn vector_same_set<T: PartialEq + Clone>(left: &Vec<T>, right: &Vec<T>)
     -> bool {
 
     if left.len() != right.len() {
@@ -31,7 +30,7 @@ fn vector_same_set<T: PartialEq + Clone>(left: &Vec<T>, right: &Vec<T>)
     }
 
     let mut temp_left = left.to_vec();
-    let mut temp_right = left.to_vec();
+    let mut temp_right = right.to_vec();
 
     for item in temp_left.drain(..) {
         let mut found_item = false;
@@ -89,6 +88,7 @@ pub fn best_knapsack(mut problem: KnapsackProblem)
             capacity: problem.capacity - test_item.weight,
             options: other_items,
         }) {
+            /* TODO -- Question, what should be done in case of integer overflow? */
             Ok(mut solution) =>
                 if (solution.value + test_item.value) > without_item.value {
                     solution.weight += test_item.weight;
@@ -110,6 +110,10 @@ pub fn best_knapsack(mut problem: KnapsackProblem)
 #[cfg(test)]
 mod tests {
     use super::*;
+    extern crate rand;
+    use self::rand::Rng;
+    const MAX_VECTOR_SIZE: usize = 30;
+
     #[test]
     fn it_works() {
     }
@@ -148,11 +152,118 @@ mod tests {
         assert_eq!(best_knapsack(knapsack_problem).unwrap(), correct_solution);
     }
 
+    /*
+     * NOTE:
+     *  This function returns Items with attributes in the 16 bit range.
+     *  This is so that with 30 elements in a knapsack, it should be impossible for the value or
+     *  weight to overflow a 32 bit unsigned integer.
+     */
+    impl rand::Rand for Item {
+        fn rand<R: rand::Rng>(rng: &mut R) -> Item {
+            let tuple: (u16, u16) = rng.gen();
+            Item { weight: tuple.0 as u32, value: tuple.1 as u32 }
+        }
+    }
+
+    /* Thoughts on what might be interesting:
+     * If I make the distribution less random and more weighted to the edges, this may catch more
+     * edge cases in the code.
+     * See https://doc.rust-lang.org/rand/rand/distributions/gamma/index.html
+     * https://doc.rust-lang.org/rand/rand/distributions/index.html
+     */
+    fn random_vector<T: rand::Rand>(max_length: usize)
+        -> Vec<T> {
+            if max_length == 0 {
+                return Vec::new();
+            }
+
+            let length: usize = rand::random::<usize>() % max_length;
+            let mut retval: Vec<T> = Vec::with_capacity(length);
+            let mut rng = rand::thread_rng();
+            for _ in 0..length {
+                retval.push(rng.gen());
+            }
+            retval
+    }
+
+    #[test]
+    fn same_result_each_time() {
+        let rand_vec = random_vector(rand::random::<usize>() %
+                                               MAX_VECTOR_SIZE);
+        let random_capacity = rand::random::<u32>();
+
+        let original_answer = best_knapsack(KnapsackProblem {
+            capacity: random_capacity,
+            options: rand_vec.clone(),
+        });
+        let other_answer = best_knapsack(KnapsackProblem {
+            capacity: random_capacity,
+            options: rand_vec.clone(),
+        });
+
+        assert_eq!(other_answer, original_answer);
+    }
+
+    #[allow(dead_code)]
+    struct VectorPermutations<T> {
+        permutation: Vec<T>,
+        count: Vec<u32>,
+        started: bool,
+        index: u32,
+    }
+
+    // impl Iterator for VectorPermutations<T> {
+    //     type Item = &Vec<T>;
+
+    //     // // Implement Heap's algorithm for permutations
+    //     // fn next(&mut self) -> Option<&Vec<T>> {
+    //     //     // Start and end conditions
+    //     //     let num_elements = self.permutation.len();
+    //     //     if !self.started {
+    //     //         if permutation.is_empty() {
+    //     //             return None;
+    //     //         }
+    //     //         self.started = true;
+    //     //         // Return original permutation as first one.
+    //     //         Some(&self.permutation)
+    //     //     } else if self.index == num_elements {
+    //     //         // This is our finished condition
+    //     //         None
+    //     //     } else {
+    //     //         // Move one more step along Heap's algorithm
+    //     //         if self.count[self.index] < self.index {
+    //     //         }
+    //     //     }
+            
+    //     }
+    // }
+
+    #[allow(dead_code)]
+    impl <T> VectorPermutations<T> {
+        fn from_vec(original_vector: Vec<T>)
+            -> VectorPermutations<T> {
+                let len = original_vector.len();
+                VectorPermutations {
+                    permutation: original_vector,
+                    count: vec![0; len],
+                    started: false,
+                    index: 0,
+                }
+        }
+    }
+
     // #[test]
     // fn order_insensitive() {
-    //     let item_options = // Generate random vector
-    //         ;
-    //     let mut all_permutations = item_options.permutations();
+    //     let max_weight = rand::random();
+    //     let max_value = rand::random();
+    //     let max_length = rand::random() % 10;
+    //     let item_options = random_item_vector(max_weight, max_value, max_length);
+    //     /* 
+    //      * Have a random set of Items.
+    //      * Create an iterator over each permutation of items.
+    //      * For each permutation, check that my function gives the same result.
+    //      */
+    //     let mut permutations = VectorPermutations::from_vec(item_options);
     //     let first_solution = all_permutations.next();
     //     for solution in all_permutations {
     //         assert_eq!(solution.weight, 8);
@@ -162,6 +273,50 @@ mod tests {
     //             assert!(solution.items.contains(&item));
     //         }
     //     }
+    // }
+
+    fn alternate_same_set<T: PartialEq + Ord>(left: &mut Vec<T>, right: &mut Vec<T>) -> bool {
+        left.sort(); right.sort();
+        left == right
+    }
+
+    #[test]
+    fn set_fail_comparison() {
+        let left = vec![false, true];
+        let right = vec![false, false];
+        assert!(!vector_same_set(&left, &right));
+    }
+
+    #[test]
+    fn set_comparison() {
+        let mut left: Vec<bool> = random_vector(MAX_VECTOR_SIZE);
+        let vector_length = left.len();
+        let mut right = left.clone();
+        if vector_length != 0 {
+            right[rand::random::<usize>() % vector_length] = rand::random::<bool>();
+        }
+        println!("Original Vectors:\n\t{:?}\n\t{:?}", left, right);
+        let originally_same = vector_same_set(&mut left, &mut right);
+        let after_same = alternate_same_set(&mut left, &mut right);
+        assert!(after_same == originally_same,
+                "left: {:?}    right: {:?}\n\
+                Fast version: {:?}, Slow Version: {:?}", left, right,
+                originally_same, after_same);
+    }
+
+//     #[test]
+//     fn set_check_permutation_independent() {
+//         let initial_random = random_vector(MAX_VECTOR_SIZE);
+//         let test_vector = rand_vec.clone();
+//         for vector_permutation in /* Permutations of original vector */ {
+//             assert!(vector_same_set(test_vector, vector_permutation));
+//         }
+
+//         let alt_rand_vec = random_vector(MAX_VECTOR_SIZE);
+//         let is_same_set = vector_same_set(alt_rand_vec, test_vector);
+//         for alt_vector_permutations in /* Permutations of new vector */ {
+//             assert_eq!(vector_same_set(alt_vector_permutations, test_vector), is_same_set);
+//         }
     // }
 
 }
