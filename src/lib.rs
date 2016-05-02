97ec9ccf31a954c02d7e84a0914de72055873f01
hardenedapple <hardenedapple@server.fake>
2016-05-02 13:59:38 +0100

    Get VectorPermutations working
    Haven't yet tested whether it works in all cases, but it appears to
    work, and it compiles and returns vectors.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,33 @@
 /*
+ * TODO Documentation tests -- see here
+ *      https://doc.rust-lang.org/book/testing.html#documentation-tests
+ *
+ *      Testing the tests
+ *          Not sure about testing that random_vector() returns an actually random vector
+ *              Seems like a lot of trouble to get too few assertions.
+ *
+ *          Testing the VectorPermutations stuff should be reasonably straightforward for small
+ *          vectors, but I'd like to push the test as far forward as possible.
+ *              First guess:
+ *                  Test that there are no duplicates in any permutation of [1, 2, 3, 4]
+ *                  Ensure that each element is in each position (N-1)! times
+ *                      Array counting the number of times each element has been in a position.
+ *
+ *              Alternative test:
+ *                  Ensure that all permutations are reached for N == 3.
+ *                  For each N onwards (up to 10), ensure that each element is put in the last
+ *                  position N-1 times.
+ *                  Doesn't actually proove the algorithm has been implemented correctly, but
+ *                  assuming the subset of the array reaches each permutation correctly it works.
+ *
+ *              Third alternative:
+ *                  Functionally implement the recursive version of Heap's algorithm.
+ *                  Test this recursive version, using the induction hypothesis (which we can
+ *                  proove applies because we're reusing the same code without holding state).
+ *                  Compare the results between each version.
+ *
  * vimcmd: set makeprg=cargo\ test
- * vimcmd: !cargo test
+ * vimcmd: !cargo test -- --ignored
  */
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Item {
@@ -22,7 +49,7 @@ pub struct KnapsackSolution {
     items: Vec<Item>,
 }
 
-pub fn vector_same_set<T: PartialEq + Clone>(left: &Vec<T>, right: &Vec<T>)
+fn vector_same_set<T: PartialEq + Clone>(left: &Vec<T>, right: &Vec<T>)
     -> bool {
 
     if left.len() != right.len() {
@@ -49,6 +76,13 @@ pub fn vector_same_set<T: PartialEq + Clone>(left: &Vec<T>, right: &Vec<T>)
     }
 
     return true;
+}
+
+/* Allow access to vector_same_set() from inside the test module. */
+#[cfg(test)]
+pub fn vector_same_set_test<T: PartialEq + Clone>(left: &Vec<T>, right: &Vec<T>)
+    -> bool {
+    vector_same_set(left, right)
 }
 
 impl PartialEq for KnapsackSolution {
@@ -112,6 +146,7 @@ mod tests {
     extern crate rand;
     use self::rand::Rng;
     const MAX_VECTOR_SIZE: usize = 30;
+    const MAX_PERMUTATION_SIZE: usize = 10;
 
     #[test]
     fn it_works() {
@@ -156,6 +191,8 @@ mod tests {
      *  This function returns Items with attributes in the 16 bit range.
      *  This is so that with 30 elements in a knapsack, it should be impossible for the value or
      *  weight to overflow a 32 bit unsigned integer.
+     *  TODO -- actually check that this is the case, so far this conclusion is only justified by
+     *  inspection.
      */
     impl rand::Rand for Item {
         fn rand<R: rand::Rng>(rng: &mut R) -> Item {
@@ -164,11 +201,13 @@ mod tests {
         }
     }
 
-    /* Thoughts on what might be interesting:
+    /*
+     * TODO
      * If I make the distribution less random and more weighted to the edges, this may catch more
      * edge cases in the code.
      * See https://doc.rust-lang.org/rand/rand/distributions/gamma/index.html
      * https://doc.rust-lang.org/rand/rand/distributions/index.html
+     * for how to select alternate distributions.
      */
     fn random_vector<T: rand::Rand>(max_length: usize)
         -> Vec<T> {
@@ -203,41 +242,13 @@ mod tests {
         assert_eq!(other_answer, original_answer);
     }
 
-    #[allow(dead_code)]
     struct VectorPermutations<T> {
         permutation: Vec<T>,
-        count: Vec<u32>,
+        count: Vec<usize>,
         started: bool,
-        index: u32,
+        index: usize,
     }
 
-    // impl Iterator for VectorPermutations<T> {
-    //     type Item = &Vec<T>;
-
-    //     // // Implement Heap's algorithm for permutations
-    //     // fn next(&mut self) -> Option<&Vec<T>> {
-    //     //     // Start and end conditions
-    //     //     let num_elements = self.permutation.len();
-    //     //     if !self.started {
-    //     //         if permutation.is_empty() {
-    //     //             return None;
-    //     //         }
-    //     //         self.started = true;
-    //     //         // Return original permutation as first one.
-    //     //         Some(&self.permutation)
-    //     //     } else if self.index == num_elements {
-    //     //         // This is our finished condition
-    //     //         None
-    //     //     } else {
-    //     //         // Move one more step along Heap's algorithm
-    //     //         if self.count[self.index] < self.index {
-    //     //         }
-    //     //     }
-            
-    //     }
-    // }
-
-    #[allow(dead_code)]
     impl <T> VectorPermutations<T> {
         fn from_vec(original_vector: Vec<T>)
             -> VectorPermutations<T> {
@@ -249,41 +260,66 @@ mod tests {
                     index: 0,
                 }
         }
+
+        fn permute<'a>(&'a mut self) -> Option<&'a Vec<T>> {
+            // Start and end conditions
+            let num_elements = self.permutation.len();
+            if !self.started {
+                if self.permutation.is_empty() {
+                    return None;
+                }
+                self.started = true;
+                // Return original permutation as first one.
+                return Some(&self.permutation);
+            } else if self.index == num_elements {
+                // This is our finished condition
+                return None;
+            }
+
+            // Move one more step along Heap's algorithm
+            while self.index < num_elements {
+                if self.count[self.index] < self.index {
+                    let swap_index = self.index % 2 * self.count[self.index];
+                    self.permutation.swap(swap_index, self.index);
+                    self.count[self.index] += 1;
+                    self.index = 1;
+                    return Some(&self.permutation);
+                } else {
+                    self.count[self.index] = 0;
+                    self.index += 1;
+                }
+            }
+            None
+        }
     }
 
-    // #[test]
-    // fn order_insensitive() {
-    //     let max_weight = rand::random();
-    //     let max_value = rand::random();
-    //     let max_length = rand::random() % 10;
-    //     let item_options = random_item_vector(max_weight, max_value, max_length);
-    //     /* 
-    //      * Have a random set of Items.
-    //      * Create an iterator over each permutation of items.
-    //      * For each permutation, check that my function gives the same result.
-    //      */
-    //     let mut permutations = VectorPermutations::from_vec(item_options);
-    //     let first_solution = all_permutations.next();
-    //     for solution in all_permutations {
-    //         assert_eq!(solution.weight, 8);
-    //         assert_eq!(solution.value, 15);
-    //         assert_eq!(solution.capacity, 7);
-    //         for item in &other_items[1..] {
-    //             assert!(solution.items.contains(&item));
-    //         }
-    //     }
-    // }
+    #[test]
+    #[ignore]
+    fn order_insensitive() {
+        let item_options: Vec<Item> = random_vector(MAX_PERMUTATION_SIZE);
+        let mut permutations = VectorPermutations::from_vec(item_options);
+        let knapsack_capacity = rand::random();
+        let first_solution: KnapsackSolution;
+        if let Some(initial_permutation) = permutations.permute() {
+            first_solution = best_knapsack(KnapsackProblem {
+                capacity: knapsack_capacity,
+                options: initial_permutation.clone(),
+            }).unwrap();
+        } else {
+            return;
+        }
+
+        while let Some(next_permutation) = permutations.permute() {
+            assert_eq!(first_solution, best_knapsack(KnapsackProblem {
+                capacity: knapsack_capacity,
+                options: next_permutation.clone(),
+            }).unwrap());
+        }
+    }
 
     fn alternate_same_set<T: PartialEq + Ord>(left: &mut Vec<T>, right: &mut Vec<T>) -> bool {
         left.sort(); right.sort();
         left == right
-    }
-
-    #[test]
-    fn set_fail_comparison() {
-        let left = vec![false, true];
-        let right = vec![false, false];
-        assert!(!vector_same_set(&left, &right));
     }
 
     #[test]
@@ -296,7 +332,7 @@ mod tests {
             right = random_vector(MAX_VECTOR_SIZE);
         }
         println!("Original Vectors:\n\t{:?}\n\t{:?}", left, right);
-        let originally_same = vector_same_set(&mut left, &mut right);
+        let originally_same = vector_same_set_test(&mut left, &mut right);
         let after_same = alternate_same_set(&mut left, &mut right);
         assert!(after_same == originally_same,
                 "left: {:?}    right: {:?}\n\
@@ -304,19 +340,24 @@ mod tests {
                 originally_same, after_same);
     }
 
-//     #[test]
-//     fn set_check_permutation_independent() {
-//         let initial_random = random_vector(MAX_VECTOR_SIZE);
-//         let test_vector = rand_vec.clone();
-//         for vector_permutation in /* Permutations of original vector */ {
-//             assert!(vector_same_set(test_vector, vector_permutation));
-//         }
+    #[test]
+    #[ignore]
+    fn set_check_permutation_independent() {
+        let initial_random: Vec<u32> = random_vector(MAX_PERMUTATION_SIZE);
+        println!("Initial vector: {:?}", initial_random);
+        let test_vector = initial_random.clone();
+        let mut vector_permutations = VectorPermutations::from_vec(initial_random);
+        while let Some(new_permutation) = vector_permutations.permute() {
+            assert!(vector_same_set_test(&test_vector, &new_permutation));
+        }
 
-//         let alt_rand_vec = random_vector(MAX_VECTOR_SIZE);
-//         let is_same_set = vector_same_set(alt_rand_vec, test_vector);
-//         for alt_vector_permutations in /* Permutations of new vector */ {
-//             assert_eq!(vector_same_set(alt_vector_permutations, test_vector), is_same_set);
-//         }
-    // }
+        let alt_rand_vec: Vec<u32> = random_vector(MAX_PERMUTATION_SIZE);
+        println!("Alternate vector: {:?}", alt_rand_vec);
+        let is_same_set = vector_same_set_test(&alt_rand_vec, &test_vector);
+        vector_permutations = VectorPermutations::from_vec(alt_rand_vec);
+        while let Some(new_permutation) = vector_permutations.permute() {
+            assert_eq!(vector_same_set_test(new_permutation, &test_vector), is_same_set);
+        }
+    }
 
 }
