94ae09723288ce6d62723012b67130229048d31a
Matthew Malcomson <hardenedapple@gmail.com>
2017-10-31 17:07:32 +0000

    Implement .keys() .values() and .remove()
    Have gone back to Vec<T> as that has a .remove() method that makes
    things easier to implement.
    
    OrderedDict.remove() is currently O(n), but it should be O(1).
    Will have to make our own implementation of a Linked List in order to
    get the proper complexity profile on the OrderedDict .remove() method.

--- src/ordered_dict/mod.rs
+++ src/ordered_dict/mod.rs
@@ -1,12 +1,14 @@
-use std::collections::{ HashMap, LinkedList };
+use std::collections:: HashMap;
 use std::rc::Rc;
 use std;
 
 // Alternate thoughts
 //
 // Maybe I should use a vector for cache optimisation and to allow random access.
-// The random access isn't really that useful at the moment, and it's entirely possible that the
-// LinkedList implementation does something clever to try and optimise for caches.
+//  Using a Vec[] would mean that I don't have to store a pointer to a LinkedList element, but
+//  could simply store an index into the vector.
+//  It's essentially the same thing though, so I don't think it's *bette* so much as more
+//  convenient.
 
 // TODO
 //  Things I don't really like:
@@ -15,7 +17,7 @@ use std;
 pub struct OrderedIter<'a, K: 'a, V: 'a> 
     where K: std::cmp::Eq + std::hash::Hash {
         // TODO Any way to make this neater?
-        order_iter: std::collections::linked_list::Iter<'a, Rc<K>>,
+        order_iter: std::slice::Iter<'a, Rc<K>>,
         underlying_hash: &'a HashMap<Rc<K>, V>,
 }
 
@@ -36,6 +38,35 @@ impl<'a, K, V> Iterator for OrderedIter<'a, K, V>
     fn size_hint(&self) -> (usize, Option<usize>) { self.order_iter.size_hint() }
 }
 
+pub struct OrderedKeys<'a, K: 'a, V: 'a>
+    where K: std::cmp::Eq + std::hash::Hash {
+        underlying: OrderedIter<'a, K, V>
+    }
+
+impl<'a, K, V> Iterator for OrderedKeys<'a, K, V>
+    where K: std::cmp::Eq + std::hash::Hash {
+        type Item = &'a K;
+        fn next(&mut self) -> Option<Self::Item> {
+            self.underlying.next().map(|x| x.0)
+        }
+        fn size_hint(&self) -> (usize, Option<usize>) { self.underlying.size_hint() }
+    }
+
+pub struct OrderedValues<'a, K: 'a, V: 'a>
+    where K: std::cmp::Eq + std::hash::Hash {
+        underlying: OrderedIter<'a, K, V>
+    }
+
+impl<'a, K, V> Iterator for OrderedValues<'a, K, V>
+    where K: std::cmp::Eq + std::hash::Hash {
+        type Item = &'a V;
+        fn next(&mut self) -> Option<Self::Item> {
+            self.underlying.next().map(|x| x.1)
+        }
+        fn size_hint(&self) -> (usize, Option<usize>) { self.underlying.size_hint() }
+    }
+
+
 // TODO
 //      Allow deletion of elements in the hash map (i.e. use LinkedList<>)
 //      Flesh out the interface by adding IntoIter implementations etc.
@@ -46,15 +77,35 @@ impl<'a, K, V> Iterator for OrderedIter<'a, K, V>
 // --------------------------------------------------------------------------------
 
 pub struct OrderedDict {
+    // map of keys to values
     underlying: HashMap<Rc<String>, usize>,
-    order: LinkedList<Rc<String>>,
+    // map of keys to positions in the vector
+    map: HashMap<Rc<String>, usize>,
+    // TODO Implement my own doubly linked list.
+    // That way I can rely on the implementation, and store Shared<T> pointers in the rest of the
+    // OrderedDict structure.
+    // When storing Shared<T> pointers I can make remove() O(1)
+    //  This would be done in the same way as the reference python implementation of OrderedDict.
+    //  That class stores 
+    //      1) an underlying dictionary of keys and values.
+    //      2) a map of keys to LinkedList nodes
+    //      3) a linked list of elements
+    // When removing a key, we use the map of keys to nodes in order to find the node to remove in
+    // O(1) time.
+    //
+    // At the moment I use Vec<T>, which means the removal is O(n).
+    // I can't use the provided LinkedList structure as the structure of the nodes is an
+    // implementation detail, and there's no supported way to say "remove the element I have a
+    // reference to".
+    order: Vec<Rc<String>>,
 }
 
 impl OrderedDict {
     pub fn new() -> OrderedDict {
         OrderedDict {
             underlying: HashMap::new(),
-            order: LinkedList::new()
+            map: HashMap::new(),
+            order: Vec::new(),
         }
     }
     pub fn insert(&mut self, k: String, v: usize) -> Option<usize> {
@@ -62,7 +113,8 @@ impl OrderedDict {
         match self.underlying.insert(refcell.clone(), v) {
             Some(v) => Some(v),
             None => {
-                self.order.push_back(refcell.clone());
+                self.order.push(refcell.clone());
+                self.map.insert(refcell.clone(), self.order.len() - 1);
                 None
             }
         }
@@ -74,17 +126,27 @@ impl OrderedDict {
             underlying_hash: &self.underlying
         }
     }
-    // pub fn remove(&mut self, k: &String) -> Option<usize> {
-    //     // TODO Have to do something special here ...
-    // }
-    // fn keys(&self) -> OrderedKeys { OrderedKeys::new(self.order) }
-    // fn values(&self) -> OrderedValues ..
+    // TODO This will change when I've implemented a LinkedList that I can rely on the internal
+    // structure of.
+    pub fn remove(&mut self, k: &String) -> Option<usize> {
+        self.map.remove(k)
+            .map(|v| Some(self.order.remove(v)));
+        self.underlying.remove(k)
+    }
+    pub fn keys(&self) -> OrderedKeys<String, usize> {
+        OrderedKeys {
+            underlying: self.iter()
+        }
+    }
+    pub fn values(&self) -> OrderedValues<String, usize> {
+        OrderedValues { underlying: self.iter() }
+    }
     // fn iter_mut(&mut self)
     // fn keys_mut(&mut self)
     // fn values_mut(&mut self)
     // fn entry(&mut self, key: K)
-    // fn len(&self) -> usize
-    // fn is_empty(&self) -> bool
+    pub fn len(&self) -> usize { self.order.len() }
+    pub fn is_empty(&self) -> bool { self.order.is_empty() }
     // fn drain(&mut self) -> Drain<K, V>
     // fn clear(&mut self)
     // fn contains_key(&self, k: &Q)
@@ -123,11 +185,34 @@ mod tests {
         do_check(&mydict, String::from("Other test"), 2, 6);
         do_check(&mydict, String::from("Test string"), 1, 15);
         do_check(&mydict, String::from("Other test"), 2, 6);
+
+        let keys_in_order = mydict.keys()
+            .map(|x| x.clone())
+            .collect::<Vec<_>>();
+        assert_eq!(keys_in_order,
+            vec![String::from("Hello world"),
+                String::from("Test string"),
+                String::from("Other test"),
+            ]);
+
+        let values_in_order = mydict.values()
+            .map(|x| *x)
+            .collect::<Vec<_>>();
+        assert_eq!(values_in_order, vec![10, 15, 6]);
+
+        let items_in_order = mydict.iter()
+            .map(|(s, u)| (s.clone(), *u))
+            .collect::<Vec<_>>();
+        assert_eq!(items_in_order,
+            vec![(String::from("Hello world"), 10),
+                (String::from("Test string"), 15),
+                (String::from("Other test"), 6),
+            ]);
     }
 
     #[test]
     #[should_panic(expected = "Could not retrieve value from OrderedDict")]
-    fn can_mark_missing() {
+    fn can_see_missing() {
         let mut mydict = OrderedDict::new();
         mydict.insert(String::from("Other test"), 6);
         do_check(&mydict, String::from("Other tst"), 2, 6);
@@ -135,11 +220,12 @@ mod tests {
 
     #[test]
     #[should_panic(expected = "\"2\"th element did not match")]
-    fn can_mark_false() {
+    fn can_see_false() {
         let mut mydict = OrderedDict::new();
         mydict.insert(String::from("Other test"), 6);
         do_check(&mydict, String::from("Other test"), 2, 7);
     }
+
 }
 
 
